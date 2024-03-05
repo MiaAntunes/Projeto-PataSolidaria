24868e069aef927a2fd93463b0962b6e4e3deee6
Camila Antunes <107724685+MiaAntunes@users.noreply.github.com>
2024-03-05 17:57:08 -0300

    CONFIG: estruturando e implementando as configurações da página

---
+++ pata-solidaria/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Router} from 'react-router-dom'
+import { AppRoutes } from './router/router'
+
+function App() {
+
+  return (
+    <>
+      <AppRoutes/>
+    </>
+  )
+}
+
+export default App
---
+++ pata-solidaria/src/router/coordinator.jsx
@@ -0,0 +1,7 @@
+export const goToHomePage = (navigate) => {
+    return navigate('')
+}
+
+export const goToPostIdPage = (navigate) => {
+    return navigate(`/contact`)
+}
